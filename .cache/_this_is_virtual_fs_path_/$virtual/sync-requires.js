fd26df6f1e7af84469058f80f0ef59b66be06fcc
jhonattan <jhonattan.benitez@hotmail.com>
2021-04-21 20:45:22 -0500

    blog added

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -8,6 +8,7 @@ exports.components = {
   "component---src-pages-404-js": preferDefault(require("D:\\Proyectos\\gatsby-portfolio-v2\\src\\pages\\404.js")),
   "component---src-pages-blog-js": preferDefault(require("D:\\Proyectos\\gatsby-portfolio-v2\\src\\pages\\blog.js")),
   "component---src-pages-index-js": preferDefault(require("D:\\Proyectos\\gatsby-portfolio-v2\\src\\pages\\index.js")),
-  "component---src-pages-projects-js": preferDefault(require("D:\\Proyectos\\gatsby-portfolio-v2\\src\\pages\\projects.js"))
+  "component---src-pages-projects-js": preferDefault(require("D:\\Proyectos\\gatsby-portfolio-v2\\src\\pages\\projects.js")),
+  "component---src-templates-blog-template-js": preferDefault(require("D:\\Proyectos\\gatsby-portfolio-v2\\src\\templates\\blog-template.js"))
 }
 
